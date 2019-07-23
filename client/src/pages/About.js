e1a37ebdecc3b1f156f13e5e3f2ef26e03024b5f
dansirdan <daniel.monteton@gmail.com>
2019-07-23 13:29:43 -0600

    wrapped Google Api in string literal, removed audition on about, edited register, fixed an error on autoComplete

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col } from "react-bootstrap"
 import { Container } from "../components/Grid";
-import {Audition} from "../components/PostType"
 
 class About extends Component {
 
@@ -13,7 +12,6 @@ class About extends Component {
   render() {
     return (
       <div className="about-section">
-        <Audition />
         <Container>
           <Row className="justify-content-md-center">
 
@@ -21,12 +19,12 @@ class About extends Component {
               <h2 className="">About Us</h2>
               <br />
               <p className="text-black">
-                Ever since my first experience in Utah as a student attending the University of Utah,
-                I knew Utah was a special place for those pursuing a career in dance. From a diverse
-                range of colleges, each offering a degree in dance to the many professional companies in
-                Utah, including Ririe-Woodbury, Repertory Dance Theater, Ballet West, Odyssey Dance Theater,
-                and so many more. Not only are there so many classes, auditions, and companies to keep track of
-                there is the schedule for each organization's performance calendar.
+                Salt Lake City is home to one of the most diverse ranges of full-time and part-time professional
+                dance companies. From Ririe-Woodbury, Repertory Dance Theater, Ballet West, Odyssey Dance Theater,
+                and so many more, the professional and pre-professional dancer must wade and search through multiple
+                sites each week if they wish to find out about a Class, Performance, or Audition. Add in Facebook's
+                chaotic and confusing group system and you'll find that information on available events for dancers
+                is sorely lacking.
             </p>
               <p className="accent-text sm-text align-text-right">-Dan Mont-Eton</p>
               <hr className="my-4" />
@@ -34,6 +32,9 @@ class About extends Component {
                 That's where we come in. Dance Salt Lake displays all of Salt Lake's regional dance information
                 for aspiring dancers throughout the valley. Individual Artists and Organizations may sign up for
                 a secure account that allows them to easily input information and send the word out to the community.
+                Our app cuts the confusion and presents the events in an easy to navigate single page application. Even
+                if your Class, Performance, or Audition takes place over multiple days, our copy-event method allows
+                users to easily duplicate and edit their events.
             </p>
             </Col>
 
@@ -48,14 +49,14 @@ class About extends Component {
               <br />
               <p className="text-black">
                 I am deeply passionate about people and technology. My experience from the dance world has shaped me into a highly creative, collaborative, and adaptive developer. I enjoy resolving coding errors, creating exciting and interactive user interfaces, and love that I still get to collaborate with people who are just as excited about Web Development as I am.
-                </p> 
-                <p> 
+                </p>
+              <p>
                 I am a retired professional dancer with 10 years of experience in person to person collaboration, choreography, teaching, and performance. I have toured internationally in partnership with the US Department of State and the Ririe-Woodbury Dance Company, to Mongolia and South Korea. I have also taught extensively to all ages, abilities, and communities. Stepping into my new career as a Full Stack Web Developer has allowed me to see the many similarities between choreography and coding; between art and technology.
                 </p>
               <hr className="my-4" />
               <p className="text-black">
-                <a href="https://github.com/dansirdan">View my work</a>
-            </p>
+                <a href="https://danmonteton.herokuapp.com/">View my work</a>
+              </p>
             </Col>
             <Col md="1" />
           </Row>
@@ -68,15 +69,15 @@ class About extends Component {
               <h2 className="">Jessica Bramwell</h2>
               <br />
               <p className="text-black">
-              I’m passionate about the tools and technology that bring good businesses to good people. Having an opportunity to apply my web development skills to a project that fills an important need and serves my local arts community has been an incredibly rewarding experience. 
+                I’m passionate about the tools and technology that bring good businesses to good people. Having an opportunity to apply my web development skills to a project that fills an important need and serves my local arts community has been an incredibly rewarding experience.
             </p>
               <p className="text-black">
-              I’ve always gained the greatest satisfaction out of supporting the projects and passions of others. Through the technical skills I’ve gained as a web developer I’m able to not just offer encouragement and enthusiasm but real world solutions and tangible results. This project is the first of many. I look forward to the next opportunity to serve the vast and dynamic community of creators and entrepreneurs. 
+                I’ve always gained the greatest satisfaction out of supporting the projects and passions of others. Through the technical skills I’ve gained as a web developer I’m able to not just offer encouragement and enthusiasm but real world solutions and tangible results. This project is the first of many. I look forward to the next opportunity to serve the vast and dynamic community of creators and entrepreneurs.
             </p>
               <hr className="my-4" />
               <p className="text-black">
                 <a href="https://github.com/JessiBramwell">View my work</a>
-            </p>
+              </p>
             </Col>
             <Col md="1" />
           </Row>
